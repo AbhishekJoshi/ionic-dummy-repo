175c9f3878421b33d141af30745ddd3f4034c599
Abhishek Joshi <abhishek.joshi20@gmail.com>
2018-02-22 06:01:59 -0500

    Integrate Plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+declare var SecurityProvider;
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -15,6 +17,15 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      SecurityProvider.installIfNeededAsync().then(data => {
+        console.log('Everything is good: ' + data);
+
+      }).catch(error => {
+        console.log("Something is wrong with this device's Google Play Services");
+        console.log('What is the error?: ' + error);
+      });
+
       statusBar.styleDefault();
       splashScreen.hide();
     }).catch();
